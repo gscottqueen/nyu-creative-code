92ff4417ab4d12339729582f2031755196946e8a
gscottqueen <geoffreysqueen@gmail.com>
2022-05-03 14:20:58 -0400

    clean up a bit

--- solving-sol-lewit/65.js
+++ solving-sol-lewit/65.js
@@ -3,13 +3,19 @@ const CANVAS_WIDTH = 400;
 const CANVAS_HEIGHT = 400;
 const LINE_WEIGHT = 1;
 const NUMBER_OF_COLORS = 4;
-const NUMBER_OF_LINES = CANVAS_WIDTH/LINE_WEIGHT;
+const NUMBER_OF_LINES = CANVAS_WIDTH / LINE_WEIGHT;
 
 function setup() {
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   strokeWeight(LINE_WEIGHT);
-} // initializer
+} // initialize our canvas and settings
 
+/**
+ * Takes in an array of n numbers and number 0 through 255 to create and color
+ * our lines
+ * @param {array} coordinates - The title of the book.
+ * @param {number} color - The author of the book.
+ */
 function drawLines(coordinates, color) {
   stroke(color);
   // draw from top to bottom
@@ -19,26 +25,28 @@ function drawLines(coordinates, color) {
     coordinates[2],
     coordinates[3]
   );
+  // draw from left to right
   line(
     coordinates[1],
     coordinates[0],
     coordinates[3],
     coordinates[2]
   );
-  // draw from left to right
 }
 
-// line(x1, y1, x2, y2)
 function draw() {
   for (let i = 0; i < NUMBER_OF_LINES; i++) {
-    let randomColor = 255/NUMBER_OF_COLORS * random(1,NUMBER_OF_COLORS)
-
+    let randomColor = (255 / NUMBER_OF_COLORS) * random(1, NUMBER_OF_COLORS);
+    const max = CANVAS_WIDTH;
+    const min = 0;
     drawLines([
-      random(0, CANVAS_WIDTH),
-      0,
-      random(0, CANVAS_WIDTH),
-      400
-      ], randomColor)
-    }
+      random(min, max),
+      min,
+      random(min, max),
+      max
+    ],
+    randomColor
+    );
+  }
   noLoop();
 }
