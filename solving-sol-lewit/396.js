7f14c9df917578040b6f272464a2d1a0330d3500
gscottqueen <geoffreysqueen@gmail.com>
2022-05-05 10:11:45 -0400

    includes color and offset

--- solving-sol-lewit/396.js
+++ solving-sol-lewit/396.js
@@ -6,6 +6,7 @@ const RADIUS_1 = 70 // outside radius
 const RADIUS_2 = RADIUS_1 / 2 // inside radius
 const HORIZONTAL_CENTER = CANVAS_WIDTH/2
 const VERTICAL_CENTER = CANVAS_HEIGHT/2
+const OFFSET = RADIUS_2 / 2
 
 function setup() {
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -15,44 +16,52 @@ function draw() {
   // black five-pointed star
   push();
   star(
-      HORIZONTAL_CENTER - RADIUS_1,
-      VERTICAL_CENTER - RADIUS_1,
-      5
+      HORIZONTAL_CENTER - RADIUS_1 - OFFSET,
+      VERTICAL_CENTER - RADIUS_1 - OFFSET,
+      5,
+      'black'
     );
   pop();
 
   // yellow six-pointed star
   push();
   star(
-      HORIZONTAL_CENTER + RADIUS_1,
-      VERTICAL_CENTER - RADIUS_1,
-      6
+      HORIZONTAL_CENTER + RADIUS_1 + OFFSET,
+      VERTICAL_CENTER - RADIUS_1 - OFFSET,
+      6,
+      'yellow'
     );
   pop();
 
   // red seven-pointed star
   push();
   star(
-      HORIZONTAL_CENTER - RADIUS_1,
-      VERTICAL_CENTER + RADIUS_1,
-      7
+      HORIZONTAL_CENTER - RADIUS_1 - OFFSET,
+      VERTICAL_CENTER + RADIUS_1 + OFFSET,
+      7,
+      'red'
     );
   pop();
 
   // blue eight-pointed star
   push();
   star(
-      HORIZONTAL_CENTER + RADIUS_1,
-      VERTICAL_CENTER + RADIUS_1,
-      8
+      HORIZONTAL_CENTER + RADIUS_1 + OFFSET,
+      VERTICAL_CENTER + RADIUS_1 + OFFSET,
+      8,
+      'blue'
     );
   pop();
 }
 
-function star(x, y, numberOfPoints) {
-  let angle = TWO_PI / numberOfPoints;
-  let halfAngle = angle / 2;
+function star(x, y, numberOfPoints, color) {
+  const angle = TWO_PI / numberOfPoints;
+  const halfAngle = angle / 2;
+  // set up shape
   beginShape();
+  fill(color);
+  stroke(color);
+
   for (let a = 0; a < TWO_PI; a += angle) {
     let sx = x + cos(a) * RADIUS_1;
     let sy = y + sin(a) * RADIUS_1;
@@ -61,5 +70,6 @@ function star(x, y, numberOfPoints) {
     sy = y + sin(a + halfAngle) * RADIUS_2;
     vertex(sx, sy);
   }
+
   endShape(CLOSE);
 }
