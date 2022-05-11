22b69bf7d547c7d4e190340e1bbf5042b715682a
gscottqueen <geoffreysqueen@gmail.com>
2022-05-11 12:34:45 -0400

    finalize matrices

--- schematic-composition/index.js
+++ schematic-composition/index.js
@@ -1,28 +1,41 @@
 // Schematic Composition
 // Sophie Taeuber-Arp1933
 // Physical Dimensions: 35 3/8 x 49 1/4" (89.6 x 125 cm)
+
+// Setup Global Variables
+
+// default colors
 const BEIGE = '#ebdcc5'
 const BLACK = '#000000'
+
 // create a 2dimensional array that represents a grid layout of the
-// black box with detail from the following criteria
+// painting with detail from the following criteria
 // 0 = black square
 // 1 = beige square
 // 2 = beige square bordered
 // 3 = beige circle inside black square
 const GRID_MATRICES = [
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-[1, 1, 0, 1, 1, 1, 1, 1, 0, 3, 0, 1, 1, 0, 3, 0, 3, 0, 2, 2, 2, 2, 2, 0, 1, 1],
-[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-[1, 1, 0, 1, 1, 1, 1, 1, 0, 3, 0, 1, 1, 0, 3, 0, 3, 0, 1, 1, 1, 1, 1, 0, 1, 1],
-[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 1, 1, 1, 1, 1, 0, 1, 1],
-[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 1, 1, 1, 0, 3, 0, 1, 1],
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+[1, 1, 0, 1, 1, 1, 1, 0, 3, 0, 1, 1, 0, 3, 0, 3, 0, 2, 2, 2, 2, 2, 0, 1, 1],
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+[1, 1, 0, 1, 1, 1, 1, 0, 3, 0, 1, 1, 0, 3, 0, 3, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+[1, 1, 0, 1, 1, 1, 1, 0, 3, 0, 1, 1, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 1, 1, 1, 0, 3, 0, 1, 1],
+[1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 1, 1, 1, 0, 3, 0, 1, 1],
+[1, 1, 0, 1, 1, 1, 1, 0, 3, 0, 2, 2, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 3, 0, 1, 1],
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+[1, 1, 0, 3, 0, 3, 3, 0, 3, 0, 3, 3, 3, 3, 0, 3, 0, 1, 1, 1, 0, 3, 0, 1, 1],
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
+// Setup Global Functions
 function getMaxGridHeight() {
   // creates an array of dimension length
   // [<number-of-rows>, <number-of-cols-per-row>]
@@ -32,12 +45,15 @@ function getMaxGridHeight() {
     // ????
     GRID_MATRICES.reduce((x, y) => Math.max(x, y.length), 0)
   ];
+  // calculate the max grid height based on our dimension values
   const maxGridHeight = (windowWidth / dimensions[1]) * dimensions[0]
   return maxGridHeight
 }
 
 function setup() {
-  document.getElementsByTagName("body")[0].style = 'margin: 0px' // remove default browser margin on the body element
+  // remove default browser margin on the body element
+  document.getElementsByTagName("body")[0].style = 'margin: 0px'
+
   // we need to pass a max grid height as the scond varable to keep the dynamic
   // ratio in relationship to the amount of rows we might expect to generate
   createCanvas(windowWidth, getMaxGridHeight())
@@ -50,45 +66,56 @@ function draw() {
     // we can get a dynamic squareSize by utilizing our setup width
     // to create a responsive drawing
     const squareSize = width / GRID_MATRICES[i].length
+
     // each index in the nested array represents a col
     for (let j = 0; j < GRID_MATRICES[i].length; j++) {
       // multiply each index by size to get our x,y offsets
       const x = j * squareSize;
       const y = i * squareSize;
-      const diameter = squareSize;
-      // switch method based on case match, ie. j=1
+
+      // create a switch method based on case match of values stored in
+      // our GRID_MATRICES array, ie.j = 1
       // 0 is default since it is the most common case
       // in order to get at the value of in our 2d Array for each case
       // we need to find the coordinate in the matrices by combining row
       // and col index, ie. array[<row-index>][<col-index>]
       switch (GRID_MATRICES[i][j]) {
+        case 0:
+          // 0 = black square
+          // stroke(BLACK)
+          fill(BLACK)
+          // rect(x, y, w, h)
+          rect(x, y, squareSize, squareSize);
+          break;
         case 1:
           // 1 = beige square
           noStroke()
           fill(BEIGE)
+          rect(x, y, squareSize, squareSize);
           break;
         case 2:
           // 2 = beige square bordered
           stroke(BLACK)
           fill(BEIGE)
+          rect(x, y, squareSize, squareSize);
           break;
         case 3:
           // 3 = beige circle inside black square
+          fill(BLACK)
+          rect(x, y, squareSize, squareSize);
           fill(BEIGE)
           // circle(x, y, d)
           // we need to move the origin of the circle half its diameter
           // in the negative "x" coordinate and the positive "y" coordinate
           // to place it in the center of its square
-          circle(x - squareSize/2, y + squareSize/2, squareSize)
-          fill(BLACK)
+          circle(x + squareSize/2, y + squareSize/2, squareSize)
         default:
           // 0 = black square
-          stroke(BLACK)
-          fill(BLACK)
+          // stroke(BLACK)
+          // fill(BLACK)
+          // rect(x, y, squareSize, squareSize);
+        }
       }
-      // rect(x, y, w, h)
-      rect(x, y, squareSize, squareSize);
     }
-  }
-  noLoop()
+    noLoop()
 }
