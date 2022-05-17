c69348b890e5783cad8110fb0d3b4e3e97e6d9e6
gscottqueen <geoffreysqueen@gmail.com>
2022-05-17 13:12:01 -0400

    first one I really like

--- moire-animation/index.js
+++ moire-animation/index.js
@@ -1,44 +1,95 @@
-// set the inital states
-// initial x position
-let x = 225
-//  we will use the toggle state later to determine which
-// x direction to travel
-let toggle = false
+
+// Creates two opposing and overlapping sine waves
+// which generates moire style patterns
+
+// set up our variables and values
+let dx; // Value for incrementing x
+let yvalues; // Array of height values for the wave
+let theta = 0.0; // Start angle at 0
+
+// Try different values below for experimenting with the pattern
+const xspacing = 2; // Distance between each horizontal line
+const numberOfLines = 1000
+let amplitude = 400.0; // Height of wave
+let period = 800.0; // How many pixels before the wave repeats
+
+// Create global functions
+// Sourced from p5.js sine wave example
+// https://p5js.org/examples/math-sine-wave.html
+
+function calculateWave() {
+  // Increment theta
+  // Try different values here to change the speed for'angular velocity' here
+
+  // Theta is a common way to reference the measure of an angle
+  theta += 0.01;
+
+  // For every x value, calculate a y value with sine function
+  let x = theta;
+  for (let i = 0; i < yvalues.length; i++) {
+    yvalues[i] = sin(x) * amplitude;
+    x += dx;
+  }
+}
+
+function drawMoireWaveSet(rotate) {
+  // draw sine wave with vertex lines
+  beginShape(LINES);
+
+  // if the rotate parameter is defined,
+  // then we rotate the shape
+  // to learn more about rotate
+  // https://p5js.org/reference/#/p5/rotate
+  rotate && rotate(PI)
+
+  // iterate through the yvalues array and generate
+  // a vertex line with two vertex(x,y) cordinates
+
+  // to learn more about drawing vertex
+  // https://p5js.org/reference/#/p5/vertex
+  for (let x = 0; x < yvalues.length; x++) {
+
+    // You can change the coordinate combinations here to manipulate the lines
+    // I really just went with a trial and error approach until
+    // I got something I liked
+    vertex(x * xspacing + width + yvalues[x], height + yvalues[x]);
+    vertex(x - xspacing, yvalues[x]);
+
+    // generate a vertex line starting at the negative x coordinate
+    vertex(-x * xspacing + width + yvalues[x], height + yvalues[x]);
+    vertex(x - xspacing, yvalues[x]);
+  }
+  endShape();
+}
+
+function renderMoireWaveGroup() {
+  stroke('white')
+
+  // offset startpoint to visually center
+  translate(width / 4 + 200, height / 4)
+
+  // draw first set of moire wave patterns
+  drawMoireWaveSet()
+
+  // rotate then draw mirrored shapes
+  translate(width / 4, height / 2)
+
+  // draw second set of moire wave patterns, rotated
+  drawMoireWaveSet(rotate)
+}
 
 function setup() {
-  createCanvas(500, 500);
-  // slow down the animation with frameRate()
-  frameRate(10)
+  // remove default browser margin on the body element
+  // so our canvas fits snug to the windows 0,0 index
+  document.getElementsByTagName("body")[0].style = 'margin: 0px'
+  createCanvas(windowWidth, windowHeight);
+  dx = (PI / period) * xspacing;
+  yvalues = new Array(floor(numberOfLines));
+  frameRate(40) // slow down frame rate
 }
 
 function draw() {
-  background('white');
-  // we want to travel 25px at any moment
-  const offset = 25
-
-  // we travel positive offset and negative offset, so we need to double in our
-  // we want to increment by 10, this is a good way to get 10px of space between each elipse
-  for (let i = 0; i < width - (offset * 2); i += 10) {
-    noFill()
-    ellipse(width / 2, height / 2, i, i)
-    ellipse(x, height / 2, i, i)
-  }
-
-  if (toggle === false) {
-    x = x + 3
-  }
-
-  if (toggle === true) {
-    x = x - 3
-  }
-
-  if (x > 275) {
-    toggle = true
-  }
-
-  if (x < 225) {
-    toggle = false
-  }
-
-
+  background('black');
+  calculateWave();
+  renderMoireWaveGroup();
 }
