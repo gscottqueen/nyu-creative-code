d13a8640e2d480dae9ea97a10c42c489103b352c
gscottqueen <geoffreysqueen@gmail.com>
2022-06-03 15:42:39 -0400

    update animation and trailing ellipse

--- animated-character/index.js
+++ animated-character/index.js
@@ -4,21 +4,22 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
+const offset = 5
 const x = getRandomInt(30, 60)
 const eyeDiameter = getRandomInt(20, 60)
 const drawSecondEye = getRandomInt(0, 2)
 const mouthTiltX = getRandomInt(-10, 10)
 const mouthTiltY = getRandomInt(-10,10)
 
-
 function setup() {
-  createCanvas(200, 200);
+  document.getElementsByTagName("body")[0].style = 'margin: 0px'
+  createCanvas(windowWidth, windowHeight);
+  background('black')
 }
 
 function drawEye() {
   fill('white')
   stroke('black')
-  const offset = 5
   const xOffset = mouseX < width / 2
     ? -offset
     : offset
@@ -26,25 +27,49 @@ function drawEye() {
     ? -offset
     : offset
   if (drawSecondEye === 0) {
-    circle((width / 2), height / 2, eyeDiameter);
+    circle(
+      (width / 2),
+      height / 2,
+      eyeDiameter
+    );
     fill('black')
-    circle((width / 2) + xOffset, (height / 2) + yOffset, 10);
+    circle(
+      (width / 2) + xOffset,
+      (height / 2) + yOffset,
+      10
+    );
   } else {
-    circle((width / 2) - x, height / 2, eyeDiameter);
+    circle(
+      (width / 2) - x,
+      height / 2,
+      eyeDiameter
+    );
     fill('black')
-    circle((width / 2) - x + xOffset, (height / 2) + yOffset, 10);
+    circle(
+      (width / 2) - x + xOffset,
+      (height / 2) + yOffset,
+      10
+    );
   }
 }
-console.log(drawSecondEye)
 
 function draw() {
+  const color = random(255)
+  stroke(color)
+  ellipse(
+    mouseX,
+    mouseY,
+    random(2),
+    random(2)
+  )
   drawEye()
   if (drawSecondEye !== 0) {
     translate(x * 2, 0)
     drawEye()
     translate(-x * 2, 0)
   }
-  // line(x1, y1, x2, y2)
+  stroke('white')
+  strokeWeight(2)
   line(
     (width / 2) - x + mouthTiltX,
     (height / 2) + eyeDiameter + mouthTiltX,
