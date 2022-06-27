0e71f6df60a9fea75de5e2928e636b8f2acef4a9
gscottqueen <geoffreysqueen@gmail.com>
2022-06-27 09:59:13 -0400

    mobile ready?

--- particles/index.js
+++ particles/index.js
@@ -1,52 +1,62 @@
+// this drawing includes an audio aspect, sound on!!
+// this works best on desktop
+
 const particles = [];
-let particle;
+let particle, mobile;
 let angle = 10;
-let brightness = 255;
-let velocity = 0;
-
-function pulse(brightness) {
-  if (brightness === 255) {
-    velocity += -1;
-  }
-  if (brightness === 1) {
-    velocity += 1;
-  }
-}
-
-function gravity(position) {
-  if (Math.sign(position) === -1) {
-    position += 1;
-  } else {
-    position += -1;
-  }
-  return position
-}
-
-function density(size, positions) {
-  if (Object.values(positions).every((position) => position === 0)) {
-    console.log(size)
-    size = 0;
-  } else {
-    size += -0.001
-  }
-  return size
-}
 
 class Particle {
-  constructor(
-    pos,
-    sz,
-    // gravity
-  ) {
+  constructor(position) {
     this.name = "Ball";
+    this.position = position;
+    this.size = 1;
     this.color = 255;
-    this.sz = sz;
-    this.pos = pos;
-    // this.gravity = gravity;
+  }
+
+  handleParticleDetails(particle, i) {
+    // set particle positions x, y, and z
+    particle.pos = createVector(
+      random(-width * 2, width * 2),
+      random(-width * 2, width * 2),
+      random(-width * 2, width * 2)
+    );
+    // generate particle size
+    particle.sz = mobile ? random(10, 15) : random(30, 50);
+    // add particle to array
+    if (i) {
+			// if there is an index, it is likely part of an array so we just place it back in at that index
+      particles[i] = particle;
+    } else {
+			// if there is no index, we just need to append it to the particles array
+      particles.push(particle)
+    }
+  }
+
+  handleGravity(position) {
+		// Math.sign helps check if the number is negative
+    if (Math.sign(position) === -1) {
+			// if number is neg, add 1
+      position += mobile ? 0.1 : 0.5;
+    } else {
+			// otherwise the number is positive so we remove one
+      position += mobile ? -0.1 : -0.5;
+    }
+    return position;
   }
 }
 
+// function preload() {
+//   // Load the sound file
+//   soundFormats('mp3');
+// 	// https://archive.org/details/sleep-walk
+//   song = loadSound('sleep-walk.mp3');
+// }
+
 function setup() {
+	// sets the volume of the loaded sound
+  // outputVolume(0.1);
+	// song.play(); // autoplay
+	// song.loop();
   // remove default browser margin on the body element
   // so our canvas fits snug to the windows 0,0 index
   document.getElementsByTagName("body")[0].style = "margin: 0px";
@@ -54,43 +64,31 @@ function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   // set to degrees for easy mode
   angleMode(DEGREES);
-  camera(-width / 3, width - 500, angle);
+	// try to handle mobile somewhat, not ideal
+  mobile = windowWidth < 900 ? true : false
+  console.log(mobile)
+  mobile ?
+    camera(-width, width / 2 - 100, angle) :
+    camera(-width / 3, width - 500, angle);
 
-  // create our particles
+  // create our initial particles array
   for (let i = 0; i < 15; i++) {
     // form a new Particle object
     const particle = new Particle();
-    // set particle positions x, y, and z
-    particle.pos = createVector(
-      random(-width * 2, width * 2),
-      random(-width * 2, width * 2),
-      random(-width * 2, width * 2)
-    );
-    // generate particle size
-    particle.sz = random(30, 50);
-    // add particle to array
-    particles[i] = particle;
+    particle.handleParticleDetails(particle, i);
   }
 
-  window.setInterval(function(){
-      // for (let i = 0; i < 15; i++) {
+  // add more particles every 100ms
+  window.setInterval(function () {
     // form a new Particle object
     const particle = new Particle();
-    // set particle positions x, y, and z
-    particle.pos = createVector(
-      random(-width * 2, width * 2),
-      random(-width * 2, width * 2),
-      random(-width * 2, width * 2)
-    );
-    // generate particle size
-    particle.sz = random(30, 50);
-    // add particle to array
-    particles.push(particle)
-  // }
+    // include it's details
+    particle.handleParticleDetails(particle);
   }, 100);
-    // no need for strokes
-    noStroke();
-  }
+
+  // no need for strokes
+  noStroke();
+}
 
 function draw() {
   // set up mutable location coordinate variables
@@ -118,59 +116,36 @@ function draw() {
     locY = particle.pos.y;
     locZ = particle.pos.z;
     // move to the loc coordinates
-    push();
+    push(); // do this in a push+pop to set back origin each time
     translate(locX, locY, locZ);
-    // get pulsing light from center sphere
-    // pointLight(brightness, brightness, brightness, 0, 0, 0);
+    // set up where our light source for these particles comes from, center origin
     pointLight(255, 255, 255, 0, 0, 0);
-
     // create sphere
     sphere(particle.sz);
-    particle.pos.x = gravity(particle.pos.x)
-    particle.pos.y = gravity(particle.pos.y)
-    particle.pos.z = gravity(particle.pos.z)
-    // console.log(particle.sz)
-    if (particle.sz <= 0) {
-      // remove this particle from the array
-      particles.pop(i)
-    } else {
-      // increase denisty of particle to 0
-     particle.sz = density(particle.sz, particle.pos)
-    }
-
-    particle.sz = density(particle.sz, particle.pos)
-
-    // console.log({particles})
+    // update position adding gravity each time
+    // this will pull the particles to the center allong our axis
+    particle.pos.x = particle.handleGravity(particle.pos.x);
+    particle.pos.y = particle.handleGravity(particle.pos.y);
+    particle.pos.z = particle.handleGravity(particle.pos.z);
     pop();
   }
-  // create center aggitating, sphere
-  // push();
-  // pulse our light
-  // ambientLight(brightness, brightness, brightness);
-  // shininess(255);
-  // // what color gets reflected
-  // specularColor(255);
-  // specularMaterial(255);
-  // create a pointlight on the outside of sphere
-  // const point = random(360)
-  // pointLight(255, 255, 255, -angle, angle, angle);
-  // pointLight(255, 255, 255, angle, -angle, angle);
-  // pointLight(255, 255, 255, -point, point, point);
-  // pointLight(255, 255, 255, angle, mouseY);
-  // ambientLight(255);
-  // randomly translate coordinates to create a vibrating effect
-  // translate(random(500), random(500), random(500));
-  // create our inner sphere
+  // create our inner hypercube square
   for (i = 0; i < 4; i++) {
-    let point = i * 50;
-      for (j = 0; j < 1000; j++) {
-        push();
-        // ambientLight(brightness, brightness, brightness);
-
-        ambientLight(255);
-        translate(random(-point, point), random(-point, point), random(-point, point));
-        sphere(1);
-        pop();
+    // each time increase size by 50x
+    let coordinate = mobile ? i * 10 : i * 50;
+    // create x number of spheres per square
+    for (j = 0; j < 500; j++) {
+      push();
+      ambientLight(255);
+      // we limit the location by square dimensions
+      translate(
+        random(-coordinate, coordinate),
+        random(-coordinate, coordinate),
+        random(-coordinate, coordinate)
+      );
+      // create a single pixel sphere
+      sphere(mobile ? 0.3 : 1.5);
+      pop();
     }
   }
 
@@ -186,8 +161,5 @@ function draw() {
   sphere(width);
 
   // increment variables
-  brightness += velocity;
-  angle += 0.1;
-  // check our brightness value and handle pulsing
-  pulse(brightness);
+  angle += 0.07;
 }
