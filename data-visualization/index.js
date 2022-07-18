e9192c63ba2f61c112b88e319f64eca92ebed55c
gscottqueen <geoffreysqueen@gmail.com>
2022-07-18 09:29:48 -0400

    updated

--- data-visualization/index.js
+++ data-visualization/index.js
@@ -1,54 +1,16 @@
-let plotPoints = [];
-
-// create our particle class
-class PlotPoint {
-  constructor() {
-    this.vertex;
-    this.plotPointSize = 10;
-    this.text;
-    this.itemIndex;
-    this.location
-  }
-
-  show() {
-    noFill()
-    stroke('white')
-    console.log(this.location)
-    ellipse(this.location.x, this.location.y, this.plotPointSize)
-  }
-}
-
-// fetch our crypto data
-const CRYPTO_DATA = fetch("https://api2.binance.com/api/v3/ticker/24hr")
-  .then((response) => response.json()
-);
+// I didn't have any personal data so I am hitting an api endpoint for crypto values over the last 24hrs
+const url = "https://api2.binance.com/api/v3/ticker/24hr";
+// we will return the data and bind it to a variable
+let CRYPTO_DATA;
+// set up our adjustable values
+const maxMarketValue = 100
+const yOffset = 100
+const pointSize = 10
 
 function preload() {
-  CRYPTO_DATA.then((data) => {
-    // take a look at the console to see our JSON data
-    console.log(data);
-    // some data comes back as a string so we convert to integer with parseInt
-
-    // build PlotPoint objects with characteristics from our crypto data
-    if (data.length > 0) {
-      let itemIndex = 0
-      for (var i = 0; i < data.length; i++) {
-
-        let lastPrice = Math.round(parseInt(data[i].lastPrice))
-        // let plotPointSize = (parseInt(data[i].quoteVolume) / 10000)
-        let symbol = data[i].symbol
-
-        if (lastPrice > 0) { // we only want values greater than zero
-          plotPoints[itemIndex] = new PlotPoint();
-          plotPoints[itemIndex].text = `${symbol} at $${lastPrice}`
-          // plotPoints[itemIndex].plotPointSize = plotPointSize
-          this.itemIndex = itemIndex
-          // this.location = createVector(itemIndex * 10, lastPrice)
-          this.location = createVector(itemIndex * 10, 10, 0)
-          itemIndex++
-        }
-      }
-    }
+  // fetch api doesn't work but we can use an http call to get our data
+  httpGet(url, "json", false, function (response) {
+    CRYPTO_DATA = response;
   });
 }
 
@@ -57,16 +19,43 @@ function setup() {
   // remove default browser margin on the body element
   // so our canvas fits snug to the windows 0,0 index
   document.getElementsByTagName("body")[0].style = "margin: 0px";
-
-  console.log('sorted', plotPoints)
 }
 
 function draw() {
-  // background('black')
-  push()
-  // translate(width / 2, height / 2)
-  for (let i = 1; i < plotPoints.length; i++) {
-    plotPoints[i].show();
+  if (!CRYPTO_DATA) {
+    return; // once we have data draw will function as expected
   }
-  pop()
+  // take a look at the console to see our JSON data
+  // console.log(CRYPTO_DATA);
+  background("white");
+
+	// legend
+	text('Last Price Sold', yOffset, yOffset);
+	text('Quantity Sold', width - yOffset, height - yOffset/2);
+	text('0', yOffset, height - yOffset/2);
+
+	// offset from edge of screen
+	translate(0, -yOffset)
+
+	// loop through data to display
+  for (var i = 0; i < CRYPTO_DATA.length; i++) {
+		// some of our data comes back as string so we convert with parseInt
+		// and round to the nearest whole number
+    let lastPrice = Math.round(parseInt(CRYPTO_DATA[i].lastPrice));
+    let lastQty = Math.round(parseInt(CRYPTO_DATA[i].lastQty));
+    let symbol = CRYPTO_DATA[i].symbol;
+
+    if (lastPrice > maxMarketValue) { // for all points greater than our defined max value
+			// create a location vector
+      let location = createVector(0 + lastPrice, height - lastQty, 0)
+			// draw our ellipse
+			ellipse(location.x, location.y, pointSize, pointSize);
+			// calculate distance mins for hover effect
+      let d = dist(mouseX, mouseY, location.x, location.y - yOffset);
+			// if our calculation is less than our point size based on location we display a text label
+      if (d <= pointSize) {
+        text(`${symbol}, $${lastPrice}.00`, location.x, location.y);
+      }
+    }
+  }
 }
